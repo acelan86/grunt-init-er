03715f1b698362cdcc3b22ec69dd6a8d09b7aba9
acelan86 <rabbitlxb@qq.com>
2014-03-03 12:49:11 +0800

    update

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -6,10 +6,27 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    logo :  ' * \n' + 
+            ' *                          $$!   ;$;\n' +                      
+            ' *                    !$  $$$$  !$$$   ;;\n' +                   
+            ' *                 $ *$$;$$$$$$$$$$;*$$$\n' +                   
+            ' *                $$$$$$$$$$$$$$$$$$$$$\n' +                    
+            ' *               $$$$$$;         o$$$$$o\n' +                   
+            ' *              *$$$   *#####;     $$$$$\n' +                   
+            ' *              $$$   &#$*!###     $$$$!\n' +                   
+            ' *              $$$;  $#!!###$   ;$$$$\n' +                    
+            ' *                $$$o  ;**   !$$$$!\n' +                      
+            ' *          !$&&&&$!  o$$$$$$o;   ;$&###&!     ;o$&&##&$;\n' + 
+            ' *       ###########$ o####*  #############!  o############\n' + 
+            ' *     ;#####;        #####  $####    *####;          ####*\n' + 
+            ' *      ###########  o####   ####;    ####$  $######;o####\n' + 
+            ' *          ;*#####o ####$  ####&    !#### o####     ####\n' +  
+            ' *    ####$**&####$ ;####  o####     ####o &####$o$#####\n' +  
+            ' *   ;o########$    *###   ####!    &####   ;######&!\n' +     
+            ' *                 ###;\n' +                                 
+            ' *                  ##o\n' +                                   
+            ' *                 ;#!\n' +                                     
+            ' *                 ;\n',
     // Task configuration.
     copy: {
       cssimg : {
@@ -36,9 +53,73 @@ module.exports = function(grunt) {
         ]
       }
     },
+    jshint : {
+      options : {
+        "bitwise": false,
+        "curly": true,
+        "eqeqeq": true,
+        "forin": false,
+        "immed": true,
+        "latedef": true,
+        "newcap": true,
+        "noarg": true,
+        "noempty": true,
+        //"nonew": true,
+        "plusplus": false,
+        "regexp": true,
+        "undef": true,
+        "unused": true,
+        "strict": true,
+        "trailing": true,
+        // "camelcase": true,
+        // "quotmark": true,
+        "asi": false,
+        "boss": false,
+        "debug": false,
+        "eqnull": false,
+        "esnext": true,
+        "evil": true,
+        "es3":true,
+        "expr": true,
+        "funcscope": false,
+        "globalstrict": false,
+        "iterator": false,
+        "lastsemic": false,
+        "laxbreak": false,
+        "laxcomma": false,
+        "loopfunc": false,
+        "multistr": false,
+        "onecase": false,
+        "proto": false,
+        "regexdash": false,
+        "scripturl": true,
+        "smarttabs": false,
+        "shadow": false,
+        "sub": false,
+        "supernew": false,
+        "validthis": false,
+        "browser": true,
+        "couch": false,
+        "devel": false,
+        "node": true,
+        "nonstandard": false,
+        "rhino": false,
+        "wsh": false,
+        "worker": true,
+        "nomen": false,
+        "onevar": false,
+        "passfail": false,
+        //"white": true,
+        "maxerr": 100,
+        //"maxlen": 100,
+        "maxparams": 10,
+        "maxdepth": 10,
+        //"indent": 4
+      },
+      files: ['src/business/**/*.js'],
+    },
     concat: {
       options: {
-        banner: '<%= banner %>',
         stripBanners: true
       },
       js: {
@@ -150,7 +231,12 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        banner: '<%= banner %>'
+        banner: '/*!\n' + 
+                ' * sinaadToolkit\n' +
+                ' * @author acelan <xiaobin8[at]staff.sina.com.cn> zhouyi<zhouyi3[at]staff.sina.com.cn>\n' +
+                ' * @version 1.0.0\n' +
+                '<%= logo %>' + 
+                ' */\n',
       },
       js: {
         src : '<%= concat.js.dest %>',
@@ -175,6 +261,7 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
